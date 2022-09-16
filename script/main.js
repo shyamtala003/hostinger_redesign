6918ddfb3762516859980c20a706aadf11a7f353
shyamtala003 <shyamtala003@gmail.com>
2022-09-16 18:08:17 +0530

    navbar added

---
+++ script/main.js
@@ -0,0 +1,17 @@
+// code for menubar
+let MenuBtn = document.getElementById('menu__icon');
+let NavBar  = document.querySelector(".navbar__nav");
+
+MenuBtn.onclick = () => {
+    if(MenuBtn.getAttribute("src")=="./images/nav-icon.png")
+    {
+        NavBar.classList.add("navbar__show");
+        MenuBtn.setAttribute("src","./images/close.png")
+    }else
+    {
+        NavBar.classList.remove("navbar__show");
+        MenuBtn.setAttribute("src","./images/nav-icon.png")
+    }
+    
+}
+
