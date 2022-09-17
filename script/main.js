7d612c305de48e40ad1b8c8b1d515d6b8dd2c75e
shyamtala003 <shyamtala003@gmail.com>
2022-09-17 09:46:57 +0530

    hero section added

--- script/main.js
+++ script/main.js
@@ -14,4 +14,28 @@ MenuBtn.onclick = () => {
 }
 
 
+// code for timer
 
+var countDownDate = new Date("Sep 26, 2022 15:37:25").getTime();
+
+var x = setInterval( () => {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = ((days.toString()).length==1)?`0${days}`:`${days}`;
+  document.getElementById("hours").innerHTML = ((hours.toString()).length==1)?`0${hours}`:`${hours}`;
+  document.getElementById("minutes").innerHTML = ((minutes.toString()).length==1)?`0${minutes}`:`${minutes}`;
+  document.getElementById("seconds").innerHTML = ((seconds.toString()).length<=1)?`0${seconds}`:`${seconds}`;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
